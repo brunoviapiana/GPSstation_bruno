ba7a11f120d01a483d873281911c8bf4814d2956
brunoviapiana <brviapiana@gmail.com>
2023-04-01 21:08:31 +0044

    update

---
+++ GpsStation/Models/DispositivoModel.cs
@@ -0,0 +1,10 @@
+﻿// MODELO PARA IDENTIFICAR OS DISPOSITIVOS REMOTOS
+
+namespace GpsStation.Models
+{
+    public class DispositivoModel
+    {
+        public int codigo { get; set; }
+        public Boolean Status { get; set; }
+    }
+}
--- GpsStation/Program.cs
+++ GpsStation/Program.cs
@@ -1,7 +1,36 @@
+
+using GpsStation.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+//----------------------------------------------------------------------------------
+
+//cria objeto do tipo IConfigurationBuilder e cria ligação com appsettings.jason
+//var config = new ConfigurationBuilder()
+    //.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+   // .Build();
+
+//cria variável que recebe o valor da sessão do appsettings.json selecionada
+//var value = config.GetConnectionString("DataBase");
+
+// cria o serviço da conexão do banco de dados
+// a string de conexão fica em appsettings.jason e é passada pela variável value
+builder.Services.AddDbContext<Contexto>
+    (options => options.UseSqlServer("Data Source=ACER_B\\TEW_SQLEXPRESS;Initial Catalog=GPSStation;Integrated Security=False;Id=user;Password=1234"));
+
+//---------------------------------------------------------------------------------
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
