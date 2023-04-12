adffc654ec19d8b0db5b662ba92c2f673cc0c659
brunoviapiana <brviapiana@gmail.com>
2023-04-12 19:46:11 +0044

    update

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,6 +1,12 @@
-create table Usuario (
+create table usuario (
 administrador bit not null,
 nome varchar(50) not null,
 senha varchar(8) not null,
-Id_usuario char(36) not null primary key
+Id_usuario varchar(36) not null primary key
 )
+use gpsstation
+
+INSERT INTO usuario (administrador, nome, senha, Id_usuario)
+VALUES (0, 'John Doe', 'password', 'abc12345-def6-789g-hi01-jk234lmno567');
+
+select * from usuario
